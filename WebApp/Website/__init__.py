8d56383027a1c58205c64b8948daf84c4e6209bc
Richardctrl <113863715+Richardctrl@users.noreply.github.com>
2022-11-04 19:14:36 -0400

    Update __init__.py

--- WebApp/Website/__init__.py
+++ WebApp/Website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'richard961'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     #importing blueprints
     from .views import views
@@ -14,5 +19,22 @@ def create_app():
     #extra prefix would be added ahead of route, currently simply accessing with / (no prefix)
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
